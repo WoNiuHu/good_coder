9b545ea2ad078af70f08a3ed31f25b932028bb00
Nan <4502928292@qq.com>
2016-03-08 17:50:29 +0800

    resolve multi-thread issue
    resolve multi-thread issue

--- crawl_thread.py
+++ crawl_thread.py
@@ -21,6 +21,7 @@ class CrawlThread(threading.Thread):
         self._directory = directory
         self._max_depth = max_depth
         self.thread_stop = False
+        self.status = 0
         threading.Thread.__init__(self)
 
     def run(self):
@@ -28,11 +29,13 @@ class CrawlThread(threading.Thread):
         while not self.thread_stop:
             lock.acquire()
             if url_table.url_queue.empty():
-                log.info("url_table is empty, thread %d is exiting!" % self._id)
-                self.stop()
+                log.info("url_table is empty, thread %d is holding!" % self._id)
+                self.status = 0
                 lock.release()
+                time.sleep(1)
                 continue
             info = url_table.url_queue.get()
+            self.status = 1
             lock.release()
             url = info.get('url')
             depth = info.get('depth')
@@ -40,6 +43,7 @@ class CrawlThread(threading.Thread):
             contents = webpage_parse.getWeb(url, self._pattern, self._timeout)
             if contents is None:
                 log.info('thread-%d giving up url %s for return none')
+                self.status = 0
                 continue
             webpage_save.save_page(self._directory, url, contents)
             log.info('thread-%d saved page %s' % (self._id, url))
@@ -50,9 +54,11 @@ class CrawlThread(threading.Thread):
                 i = urlparse.urljoin(url, i)
                 if depth > self._max_depth:
                     log.info("thread-%d depth:%d, over limit, pass" % (self._id, depth))
+                    self.status = 0
                     continue
                 if(i in url_table.url_table.keys()):
                     log.info("thread-%d find duplicate url %s ignore!" % (self._id, url))
+                    self.status = 0
                     continue
                 log.info("thread-%d put url %s, depth %d to queue" % (self._id, i, depth))
                 element = {}
@@ -60,6 +66,7 @@ class CrawlThread(threading.Thread):
                 element['depth'] = depth
                 lock.acquire()
                 url_table.url_queue.put(element)
+                self.status = 0
                 lock.release()
             time.sleep(self._interval)
 
@@ -69,6 +76,23 @@ class CrawlThread(threading.Thread):
 
 def crawl(thread_num, directory, max_depth, pattern, interval, timeout):
     log.debug("now crawling with %d thread_num, %d depth, pattern %s!" % (thread_num, max_depth, pattern))
+    ThreadList = []
     for i in range(thread_num):
         MyThread = CrawlThread(i, directory, max_depth, pattern, interval, timeout)
+        ThreadList.append(MyThread)
         MyThread.start()
+    while True:
+        if url_table.url_queue.empty():
+            flag = 0
+            log.debug("checking all thread status")
+            for mythread in ThreadList:
+                log.debug("thread-%d status is %d" % (mythread._id, mythread.status))
+                if mythread.status == 1:
+                    flag = 1
+            if flag == 0:
+                for mythread in ThreadList:
+                    log.debug("stopping thread-%d" % mythread._id)
+                    mythread.stop()
+                log.info("All job finished")
+                break
+        time.sleep(1)
